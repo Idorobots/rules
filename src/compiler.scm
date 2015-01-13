a223ca8d02e71591f2842501849814daf95d207e
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-01-13 18:06:05 +0100

    Remodularized the code so it doesn't look like crap.

---
+++ src/compiler.scm
@@ -0,0 +1,34 @@
+;; Rule compiler.
+
+(load "nodes.scm")
+
+(define (compile-rule pattern action)
+  (root-node (compile-pattern pattern (node-a action))))
+
+(define (compile-pattern pattern next-node)
+  (match pattern
+    (`(and . ,_)
+     (compile-conjunction pattern next-node))
+
+    (`(reduce (,fun ,var ,acc) ,pattern)
+     ;; FIXME Don't use eval.
+     (compile-pattern pattern (node-r (eval fun) var acc next-node)))
+
+    (_ (list (node-1 pattern next-node)))))
+
+(define (compile-conjunction conj next-node)
+  ((foldl (lambda (pattern build-prev)
+            (lambda (nn)
+              (let ((n2 (node-2 nn)))
+                (append (build-prev (node-2l n2))
+                        (compile-pattern pattern n2)))))
+          (lambda (nn)
+            (compile-pattern (conjunction-first conj) nn))
+          (conjunction-rest conj))
+   next-node))
+
+(define (conjunction-first pattern)
+  (cadr pattern))
+
+(define (conjunction-rest pattern)
+  (cddr pattern))
---
+++ src/utils.scm
@@ -0,0 +1,30 @@
+;; Various utilities.
+
+(define (ref x)
+  (make-vector 1 x))
+
+(define (deref ref)
+  (vector-ref ref 0))
+
+(define (assign! ref value)
+  (vector-set! ref 0 value))
+
+(define (variable? pattern)
+  (and (symbol? pattern) (starts-with? #\? pattern)))
+
+(define (starts-with? character symbol)
+  (equal? character (car (string->list (symbol->string symbol)))))
+
+(define (complimentary f)
+  (lambda args (not (apply f args))))
+
+(define not-member (complimentary member))
+(define not-equal? (complimentary equal?))
+(define not-false? (complimentary false?))
+
+(define (partial f . args)
+  (lambda rest
+    (apply f (append args rest))))
+
+(define (tagged-list? tag list)
+  (and (list? list) (equal? (list-ref list 0) tag)))
