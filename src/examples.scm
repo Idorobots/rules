0b4cdc3235eac6b4deacf40906da4f05b46274f2
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-01-13 18:21:37 +0100

    Refactored usage examples a bit.

--- src/examples.scm
+++ src/examples.scm
@@ -2,37 +2,38 @@
 
 (reset!)
 
-(whenever (provides ?x foo)
+(whenever (provides ?m foo)
           () => (display "New foo!\n"))
 
-(whenever (and (a ?x module)
-               (provides ?x gps))
+(whenever (and (module ?m)
+               (provides ?m gps))
           () => (display "New GPS!\n"))
 
 (whenever (reduce (min ?t 0.1)
-                  (and (a ?x module)
-                       (provides ?x gps)
-                       (tolerance ?x gps ?t)))
+                  (and (module ?m)
+                       (provides ?m gps)
+                       (tolerance ?m gps ?t)))
           (?t) =>
           (display "Better GPS: ")
           (display ?t)
           (display "!\n"))
 
-(assert! (a A module))
+(assert! (module A))
 (assert! (provides A foo))
 (assert! (provides A bar))
 
-(assert! (a B module))
+(assert! (module B))
 (assert! (provides B foo))
 (assert! (provides B gps))
 
 (assert! (provides C gps))
-(assert! (a C module))
+(assert! (module C))
 
 (signal! (provides A gps))
 
-(retract! (a B module))
-(assert! (a B module))
+(retract! (module B))
+(assert! (module B))
 
-(assert! (tolerance B gps 0.01))
-(assert! (tolerance C gps 0.001))
+(assert! (tolerance C gps 0.01))
+(assert! (tolerance B gps 0.001))
+(assert! (tolerance A gps 0.0001))
