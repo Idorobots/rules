cd5d54747eef522615002c6cfba8487d18d64dbc
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-02-05 22:22:15 +0100

    Added some more examples of backward chaining.

--- src/examples.scm
+++ src/examples.scm
@@ -66,3 +66,48 @@
 (select (?m ?f)  ;; All module-function pairs.
         (and (module ?m)
              (provides ?m ?f)))
+
+;; Relational log minning!
+(reset!)
+
+(define (assert-object! obj)
+  (let ((id (gensym 'id)))
+    (map (lambda (p)
+           (eval `(assert! (,(car p) ,id ,(cdr p)))))
+         obj)))
+
+(assert-object! '((id . 0) (level . info)  (msg . "Checking core...")))
+(assert-object! '((id . 0) (level . debug) (msg . "Core #23")))
+(assert-object! '((id . 0) (level . info)  (msg . "Core ok.")))
+(assert-object! '((id . 1) (level . info)  (msg . "Checking core...")))
+(assert-object! '((id . 1) (level . error) (msg . "Core melting!")))
+(assert-object! '((id . 1) (level . debug) (msg . "Core #13")))
+(assert-object! '((id . 2) (level . info)  (msg . "Checking core...")))
+(assert-object! '((id . 2) (level . debug) (msg . "Core #25")))
+(assert-object! '((id . 2) (level . info)  (msg . "Core ok.")))
+(assert-object! '((id . 3) (level . info)  (msg . "Checking core...")))
+(assert-object! '((id . 3) (level . debug) (msg . "Core #5")))
+(assert-object! '((id . 3) (level . info)  (msg . "Core ok.")))
+
+(define (recombine-logs acc log attr val)
+  (let ((l (assoc log acc)))
+    (if l
+        (cons (cons log
+                    (list* (cons attr val)
+                           (cdr l)))
+              (filter (lambda (kv)
+                        (not-equal? log (car kv)))
+                      acc))
+        (cons (cons log (list (cons attr val)))
+              acc))))
+
+;; Get all logs related to a critical failure.
+(map cdr
+     (cdaar (select (?errors)
+                    (reduce ?errors
+                            (recombine-logs () ?log2 ?attr ?val)
+                            (and (id ?log1 ?id)
+                                 (level ?log1 error)
+                                 (msg ?log1 ?msg1)
+                                 (id ?log2 ?id)
+                                 (?attr ?log2 ?val))))))
